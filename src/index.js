ee72044260dc9f1409a90d5a3463074c3b8fbca3
Maxim Yazykov <yazykovmaxim@gmail.com>
2021-03-10 02:39:41 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let s;
+  str = str.replace(/[^{}[\]()(12)(34)(56)(77)(88)(||)]/g, '');
+  while (s != str) {
+    s = str;
+    str = str.replace(/{}|\[]|\(\)|12|34|56|77|88|\|\|/g, '')
+    }
+  return !str;
+  }
+
+
+
